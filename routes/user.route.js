59a36da172b7cb5d6c28b7bce67c5cfc1561fff2
Muhammad Fariz <mfarix730@gmail.com>
2024-05-21 18:51:43 +0700

    fixdatabase

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const verifyTokenAndRole = require('../middleware/verifyTokenAndRole');
+const controller = require ('../controller/mahasiswa.controller');
 
 /* GET home page. */
 // router.get('/not-found', function(req, res, next) {
 //   res.render('notfound');
 // });
 
-router.get('/home', (req, res) => {
-  res.render('user/home'); // Pastikan jalur ini sesuai dengan lokasi sebenarnya dari file tampilan
-}); 
+router.get('/home',verifyTokenAndRole('user'),controller.profil_mahasiswa, function(req, res) {
+  res.render('user/home');
+});
+
+// router.get('/home', verifyTokenAndRole('user'), controller.profil_mahasiswa);
 
 // router.post('/userLogin', verifyTokenAndRole(user), user.checklogin);
 
--- seeders/20240512094740-demo-user.js
+++ seeders/20240512094740-demo-user.js
@@ -3,38 +3,41 @@ const bcrypt = require("bcrypt")
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const users = [
-      {
-        email: "2211523034_muhammad@student.unand.ac.id",
-        password: await bcrypt.hash("fariz1504", 10),
-        role: "user",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: "admin@gmail.com",
-        password: await bcrypt.hash("admin", 10),
-        role: "admin",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ];
+    await queryInterface.bulkInsert("users",
+      [
+        {
+          email: "2211523034_muhammad@student.unand.ac.id",
+          password: await bcrypt.hash("fariz1504", 10),
+          role: "user",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: "admin@gmail.com",
+          password: await bcrypt.hash("admin", 10),
+          role: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {}
+    )
 
-    await queryInterface.bulkInsert("users", users);
-
-    const mahasiswas = [
-    {
-      nim: "2211523034",
-      nama: "Muhammad Fariz",
-      fakultas: "Teknologi Informasi",
-      prodi: "Sistem Informasi",
-      email: "2211523034_muhammad@student.unand.ac.id",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-    ];
-
-    await queryInterface.bulkInsert("mahasiswas", mahasiswas);
+    await queryInterface.bulkInsert("mahasiswas",
+      [
+        {
+          userid: 1,
+          nim: "2211523034",
+          nama: "Muhammad Fariz",
+          fakultas: "Teknologi Informasi",
+          prodi: "Sistem Informasi",
+          email: "2211523034_muhammad@student.unand.ac.id",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
   },
 
   async down (queryInterface, Sequelize) {
